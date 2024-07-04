b190651c30936c91c41e1971680bb30564127287
Keji8080 <kevinjimenezuk@gmail.com>
2024-07-05 01:39:06 +0200

    This is just a test from Intellij to GitHub

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLastName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setLastName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAge() {
+    }
+}
