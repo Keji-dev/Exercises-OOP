15dd3585116eb73555ae19cc19c679a29b9750e4
Keji8080 <kevinjimenezuk@gmail.com>
2024-07-05 01:57:30 +0200

    Testing intellij to github

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLastName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setLastName() {
+    }
+}
